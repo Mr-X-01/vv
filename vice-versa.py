f7845c9a92c76ad48847913e26aeb481ceb19aa8
Mr-X-01 <60137544+Mr-X-01@users.noreply.github.com>
2020-11-15 17:18:01 +0300

    Add files via upload

---
+++ vice-versa.py
@@ -0,0 +1,128 @@
+print("Maked by Mr.X™")
+print("Welcome to Vice Versa.")
+while True:
+    def add(x, y):
+       return x + y
+    def subtract(x, y):
+       return x - y
+    def multiply(x, y):
+       return x * y
+    def divide(x, y):
+       return x / y
+    print("Select operation.")
+    print("0. Exit")
+    print("1. +")
+    print("2. -")
+    print("3. *")
+    print("4. /")
+    print("<<<play .5<<<")
+    print("")
+    choice = input("Choose: ")
+    if choice == '5':
+        bi = input("1011001001001110010101110001011001000101 000110100110000010000010110010: ")
+        if not bi == 'vigenere cipher':
+            print("False, try again.")
+            print("And don't give up.")
+            break
+        if bi == 'vigenere cipher':
+            key = input("Key: ")
+            if not key == 'yalp':
+                print("False, try again.")
+                print("And don't give up.")
+                break
+            if key =='yalp':
+                transformed = input("10010011111010000000: ")
+                if not transformed == 'rot0':
+                    print("False, try again.")
+                    print("And don't give up.")
+                    break
+                if transformed == 'rot0':
+                        anwrs = input("teyx tiox tinx: ")
+                        if not anwrs == 'veni vidi vici':
+                            print("False, try again.")
+                            print("And don't give up.")
+                            break
+                        if anwrs == 'veni vidi vici':
+                            print("Thank you for this all:)")
+                            print("Find me in tele;)")
+                            tele = input("fviegsy: ")
+                            if not tele == 'mrxanon':
+                                print("False, try again.")
+                                print("And don't give up.")
+                                break
+                            if tele == 'mrxanon':
+                                print("This is my telegram:)")
+                                print("I'm waiting for you:3")
+                                print("with love @Mrxanon")
+                                break
+                        
+    if choice == '0': 
+        break
+    if choice == '1':
+        num1 = int(input("First number: "))
+        num2 = int(input("Second number: "))
+        print(num1,"+",num2,"=", add(num1,num2))
+    elif choice == '2':
+        num1 = int(input("First number: "))
+        num2 = int(input("Second number: "))
+        print(num1,"-",num2,"=", subtract(num1,num2))
+    elif choice == '3':
+        num1 = int(input("First number: "))
+        num2 = int(input("Second number: "))
+        print(num1,"*",num2,"=", multiply(num1,num2))
+    elif choice == '4':
+        num1 = int(input("First number: "))
+        num2 = int(input("Second number: "))
+        print(num1,"/",num2,"=", divide(num1,num2))
+    else:
+        import time
+        start = input
+        Sec = 0
+        while start:
+            Sec += 0.25
+            time.sleep(1)
+            if Sec == 0.25:
+                print("Scanning.")
+            if Sec == 0.50:
+                print("sCanning..")
+            if Sec == 0.75:
+                print("scAnning...")
+            if Sec == 1:
+                print("scaNning....")
+            if Sec == 1.25:
+                print("scanNing.")
+            if Sec == 1.50:
+                print("scannIng..")
+            if Sec == 1.75:
+                print("scanniNg...")
+            if Sec == 2:
+                print("scanninG....")
+            if Sec == 2.25:
+                print("Scanning.")
+            if Sec == 2.50:
+                print("sCanning..")
+            if Sec == 2.75:
+                print("scAnning...")
+            if Sec == 3:
+                print("scaNning....")
+            if Sec == 3.25:
+                print("scanNing.")
+            if Sec == 3.50:
+                print("scannIng..")
+            if Sec == 3.75:
+                print("scanniNg...")
+            if Sec == 4:
+                print("scanninG....")
+            if Sec == 4.25:
+                print("The problem are found.")
+            if Sec == 4.50:
+                print("Incorrect selection!")
+            if Sec == 4.75:
+                print("")
+                print("Idiot! You made a mistake!")
+                print("Try again.")
+                print("")
+                break
+    
+
+    
